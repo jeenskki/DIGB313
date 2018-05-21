5d5563ebdc4dddc0df3d2cf2369c739c2fec6fe6
jeenskki <madrine27@naver.com>
2018-05-22 02:58:00 +0900

    minor fixes

--- Home5/javascript/script.js
+++ Home5/javascript/script.js
@@ -1,18 +1,19 @@
-function getRandomColor() {
-  let hex = Math.floor(Math.random() * 0xFFFFFF);
-  return "#" + ("000000" + hex.toString(16)).substr(-6);
-}
-
 let count = 0;
+let z = 0;
 const rootNode = document.querySelector('#root');
 
 document.querySelector('.add-box').addEventListener('click', addBox);
 
 document.querySelector('.change-color').addEventListener('click', changeBox);
 
+function getRandomColor() {
+  let hex = Math.floor(Math.random() * 0xFFFFFF);
+  return "#" + ("000000" + hex.toString(16)).substr(-6);
+}
+
 function addBox() {
   let box = document.createElement('div');
-  let randomTopPosition = Math.floor(Math.random() * 540);
+  let randomTopPosition = Math.floor(Math.random() * 460 + 80);
   let randomLeftPosition = Math.floor(Math.random() * 640);
 
   //dynamic
@@ -21,26 +22,28 @@ function addBox() {
   box.style.top = `${randomTopPosition}px`;
   box.style.left = `${randomLeftPosition}px`;
   box.style.zIndex = count;
-  
-  //append box
-  document.getElementById('root').appendChild(box);
 
-  count++;
+  //append box
+  rootNode.appendChild(box);
 
   box.addEventListener('click', refreshBox);
+
+  count++;
 }
 
+// 박스 색상 변경
 function changeBox() {
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i <= count; i++) {
     const target = document.querySelector(`.box--${i}`);
     target.style.backgroundColor = getRandomColor();
   }
 }
 
+// 박스 클릭 시 이벤트 설정
 function refreshBox() {
   const target = event.srcElement;
   if (target.style.zIndex >= count) {
     rootNode.removeChild(target);
   }
-  target.style.zIndex = count + 1;
+  target.style.zIndex = count;
 }
